071ce5217d5f486cd9896ed360c5efd79f52a4c2
hoa0217 <48192141+hoa0217@users.noreply.github.com>
2020-04-10 00:55:38 +0900

    Add files via upload
    이스터만추가하면됨

--- main.c
+++ main.c
@@ -86,7 +86,7 @@ void add(User* ptr, int*num){
 
           (*num)++;
           printf("===============\n");
-          printf("  Successful Data Inserted \n\n");
+          printf("\tSuccessful Data Inserted \n\n");
      }
      
      else
@@ -122,33 +122,131 @@ int search(User* ptr, int*num){
 		for(i=0;i<MAX_NUM;i++){
 			if(!strcmp(name,ptr[i].name)){
 				printf("Name: %s", ptr[i].name);
-				printf("Tel: %s\n", ptr[i].number);
-				printf("Address: %s\n", ptr[i].address);
-				printf("Email: %s\n", ptr[i].email);
-				printf(" Data Found\n\n");
+				printf("\tTel: %s", ptr[i].number);
+				printf("\tAddress: %s", ptr[i].address);
+				printf("\tEmail: %s\n", ptr[i].email);
+				printf("\tSuccessful Data Found\n\n");
 				return 0;
 			}
 		}
-		printf("Not Found\n\n");
+		printf("\tNot Found\n\n");
 		return 0;
 	} 
 	//저장된 데이터가 없다면 
 	else{
-		printf(" No Data\n\n");
+		printf("\tNo Data\n\n");
 	}
 }
 
 //4번 데이터 편집  
 void edit(User* ptr, int*num){
-	printf("4번 구현\n\n");
+	char name[30];
+	int i;
+	
+	//저장된 데이터가 있다면
+	if(*num>0){
+		printf("Input Name: ");
+		scanf("%s", name);
+		for(i=0;i<MAX_NUM;i++){
+			if(!strcmp(name,ptr[i].name)){
+				printf("===============\n");
+				printf("Data found\n");
+				printf("pleas edit start!\n");
+				printf("===============\n");
+				printf("Input Name: ");
+		        scanf("%s", ptr[i].name);
+		        printf("Input Phone number: ");
+		        scanf("%s", ptr[i].number);
+		        printf("Input Address: ");
+		        scanf("%s", ptr[i].address);
+		        printf("Input Email: ");
+		        scanf("%s", ptr[i].email);
+				printf("\tData edit success!\n\n");
+				return 0;
+			}
+		}
+		printf("\tNot Found\n\n");
+		return 0;
+	} 
+	//저장된 데이터가 없다면 
+	else{
+		printf("\tNo Data\n\n");
+	}
 }
  
 //5번 데이터 삭제  
 int deleted(User* ptr, int*num){
-	printf("5번 구현\n\n");
-}
+	char name[30];
+     int i, j;
+     
+     //유저 정보가 한개라도 남아있으면
+     if (*num > 0){
+          printf("Input Name: ");
+          scanf("%s", name);
 
+          for (i = 0; i < MAX_NUM; i++){
+               //문자열이므로 비교하기위해 strcmp사용
+               if (strcmp(name, ptr[i].name) == 0){
+                    (*num)--;
+                    printf("\tSuccessful Data Deleted \n\n");
+
+
+                    //데이터가 가득 차지 않았다면
+                    if (i != MAX_NUM - 1){
+                         for (j = i; j < MAX_NUM; j++){
+                              //문자열이므로 strcpy를 사용하여 데이터 복사
+                              strcpy(ptr[j].name, ptr[j + 1].name);
+                              strcpy(ptr[j].number, ptr[j + 1].number);
+                              strcpy(ptr[j].address, ptr[j + 1].address);
+                              strcpy(ptr[j].email, ptr[j + 1].email);
+                         }
+
+                     //구조체 배열의 마지막을 NULL로 바꿈
+                     *ptr[MAX_NUM - 1].name = NULL;
+                     *ptr[MAX_NUM - 1].number = NULL;
+					 *ptr[MAX_NUM - 1].address = NULL;
+                     *ptr[MAX_NUM - 1].email = NULL;
+                }
+
+
+                //데이터가 가득 찼다면
+                else{
+                     //구조체 배열의 마지막을 NULL로 바꿈
+                     *ptr[MAX_NUM - 1].name = NULL;
+                     *ptr[MAX_NUM - 1].number = NULL;
+                     *ptr[MAX_NUM - 1].address = NULL;
+                     *ptr[MAX_NUM - 1].email = NULL;
+                }
+                return 0;
+               }
+          }
+          printf("\tNot Found \n\n");
+          return 0;
+	}
+	else{
+		printf("\tNo data \n\n");
+	}
+}
 //이스터에그  
 void egg(User* ptr, int*num){
-	printf("6번 구현\n\n"); 
+	if (*num < MAX_NUM){
+		ptr[*num].name= "HAN YEON JAE";
+		ptr[*num].number= "01033797498";
+		ptr[*num].address= "Wonju";
+		ptr[*num].email= "kevin7498@naver.com";
+		++(*num);
+		ptr[*num].name="HEO JEONG HWA";
+		ptr[*num].number="01033541083";
+		ptr[*num].address="Suncheon";
+		ptr[*num].email="gjwjdghk123@gmail.com";
+        (*num)++;
+        printf("\t this is egg!\n");
+        printf("\t please check list!\n");
+     }
+     
+     else
+          {
+          	printf("\tSorry...full\n\n");
+		  }
 }
+
